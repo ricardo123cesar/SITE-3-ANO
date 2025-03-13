4ea2371277bcc11137242af0133d25798ad7367d
ricardo123cesar <143848697+ricardo123cesar@users.noreply.github.com>
2025-03-13 19:29:28 -0300

    jh

--- script.js
+++ script.js
@@ -27,5 +27,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 
+    diminuiFonteBotao.addEventListener('click', function() {
+        tamanhoAtualFonte -=0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+
 });
 
