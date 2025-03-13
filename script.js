90cb7e0a0efd0d61e111e665f44275bdf2cfb9c8
ricardo123cesar <143848697+ricardo123cesar@users.noreply.github.com>
2025-03-13 19:16:51 -0300

    jm

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// Aguarda atÃ© que todo o conteÃºdo da pÃ¡gina (HTML, CSS, etc.) esteja carregado
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    
+
+    // Acessa o botÃ£o com o ID 'aumentar-fonte' no HTML e o armazena em uma variÃ¡vel
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    // Inicializa uma variÃ¡vel que controla o tamanho da fonte. ComeÃ§a com 1rem.
+
+    let tamanhoAtualFonte = 1;
+
+    // Adiciona um evento de clique ao botÃ£o 'aumentar-fonte'
+
+    aumentaFonteBotao.addEventListener('click', function() {
+
+        // Quando o botÃ£o for clicado, aumenta o valor de tamanhoAtualFonte em 0.1
+
+        tamanhoAtualFonte += 0.1;
+
+        // Atualiza o estilo da fonte do corpo da pÃ¡gina (document.body), alterando seu tamanho para o valor de 'tamanhoAtualFonte'
+
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+});
+
